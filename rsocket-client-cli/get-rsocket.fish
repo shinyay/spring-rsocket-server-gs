3ca8c3fda69ca661b564ec12bfab0bdc81c8889c
shinyay <shinya.com@gmail.com>
2020-04-14 23:49:32 +0900

    Modify curl option

--- rsocket-client-cli/get-rsocket.fish
+++ rsocket-client-cli/get-rsocket.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-curl https://github.com/making/rsc/releases/download/0.4.2/rsc-0.4.2.jar -o rsc.jar
+curl -L https://github.com/making/rsc/releases/download/0.4.2/rsc-0.4.2.jar -o rsc.jar
